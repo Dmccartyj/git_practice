f8ad0a7a5aa646e1b14058799f6a2106a91b2a3a
DMcCarty <73554602+Dmccartyj@users.noreply.github.com>
2021-05-20 13:59:33 -0700

    commit!

--- Seattle_FirstJavaProject/src/main/java/org/perscholas/firstproject/MainApp.java
+++ Seattle_FirstJavaProject/src/main/java/org/perscholas/firstproject/MainApp.java
@@ -3,6 +3,7 @@ package org.perscholas.firstproject;
 public class MainApp {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Initial Commit") ;
+		System.out.println("Second Commit") ;
 	}
 }
